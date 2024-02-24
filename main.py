4c086b1bdd3f38a90fcdadb2c2df920975f36192
Omonullo Raimkulov <omonnuloraimkulov@gmail.com>
2024-02-24 15:47:22 +0500

    Mission Completed

--- main.py
+++ main.py
@@ -1,30 +1,37 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
-# Define the payload for the request
-payload = {
-    "text_prompt": "Hello, ahmad kot",
-    "output": {
-        'shortened_url': "https://omonullo.uz/"
-    }
-}
+app = FastAPI()
 
+origins = ["*"]
 
-payload['output']['shortened_url'] = 'omon'
-
-response = requests.post(
-    "https://api.gooey.ai/v2/art-qr-code/",
-
-    headers={
-        "Authorization": "Bearer " + "sk-2J4ocYtNQ3tXzUOjXSYMuJciW0VE8977xacZB6WS3lbVkTl8",
-    },
-    json=payload,
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
 )
 
-if response.status_code == 200:
-    response_json = response.json()
-    if 'output' in response_json:
-        if 'shortened_url' in response_json['output']:
-            response_json['output']['shortened_url'] = 'https://omonullo.uz/'
-            print(response_json)
-else:
-    print("Error:", response.status_code, response.text)
+@app.get("/api/{value}")
+def salom_dunyo(value: str):
+
+    payload = {
+        "text_prompt": "Create Beautiful Qr code WebSite",
+        "qr_code_data": f"{value}",
+    }
+
+    response = requests.post(
+        "https://api.gooey.ai/v2/art-qr-code/",
+        headers={
+            "Authorization": "Bearer " + "sk-Z6w1g92HyXNlGwzmis2PfXW3diSc0hXORusWzZkRhhPyzE8C",
+        },
+        json=payload
+    )
+
+    url = response.json()['url']
+    print(response.json())
+    return url
+
